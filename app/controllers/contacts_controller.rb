2837fb1ee09e0b7a3b83c97c8ba6217b7ef5da6a
Ludvig Kalberg <kalbergludvig@outlook.com>
2026-03-26 14:57:39 +0100

    Add contact form, save to db, style it

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -6,5 +6,11 @@ class ContactsController < ApplicationController
   def create
     message_params = params.expect(message: [:name, :email, :content])
     @message = Message.new(message_params)
+
+    if @message.save
+      redirect_to root_path, notice: "Sent successfully."
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 end
