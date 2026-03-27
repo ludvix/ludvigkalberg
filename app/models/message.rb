ac1b0d63d110ac5482895b9d338c05bd825496c6
Ludvig Kalberg <kalbergludvig@outlook.com>
2026-03-27 08:13:33 +0100

    Add email format validation

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,2 +1,6 @@
 class Message < ApplicationRecord
+  validates :name, presence: true
+  validates :email, presence: true,
+  format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :content, presence: true
 end
